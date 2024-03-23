f1ea0558c90e50fee57d0b74efbc1b824a0372b6
DarkSwordss89125 <160292757+DarkSwordss89125@users.noreply.github.com>
2024-03-23 15:20:56 +0500

    Update proga.py

--- laba5/proga.py
+++ laba5/proga.py
@@ -1,24 +1,27 @@
-def password_generator(letters_count, numbers_count):
+def password_generator(shtuk,letters_count, numbers_count):
     import random
     import string
+    passwords = []
+    i=0
+    while True:
+        letters = string.ascii_letters
+        numbers = string.digits
 
-    letters = string.ascii_letters
-    numbers = string.digits
-
-    password = ''.join(random.choices(letters, k=letters_count) + random.choices(numbers, k=numbers_count))
-    yield password
-    
-    inv_password = ''.join([s.upper() if s.islower() else s.lower() for s in password])
-    yield inv_password
-    
-    ascii_password = list(map(ord, password))
-    yield ascii_password
+        password = ''.join(random.choices(letters, k=letters_count) + random.choices(numbers, k=numbers_count))
+        yield password
+        
+        inv_password = ''.join([s.upper() if s.islower() else s.lower() for s in password])
+        yield inv_password 
+        
+        ascii_password = list(map(ord, password))
+        yield ascii_password
 
 letters_count = int(input("Введите количество букв: "))
 numbers_count = int(input("Введите количество цифр: "))
-
-generator = password_generator(letters_count, numbers_count)
-
-print("\nИсходный пароль:", next(generator))
-print("Пароль с измененным регистром:", next(generator))
-print("Пароль в ASCII коде:", next(generator))
+shtuk = int(input("Введите количество паролей"))
+generator = password_generator(shtuk,letters_count, numbers_count)
+for i in range(5):
+    print("\nПароль",i+1)
+    print("\nИсходный пароль:", next(generator))
+    print("Пароль с измененным регистром:", next(generator))
+    print("Пароль в ASCII коде:", next(generator))
